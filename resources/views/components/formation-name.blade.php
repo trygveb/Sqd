6a3d984fd2b45357392e25dcf88dfde8fa3cdb61
Trygve <trygve.botnen@gmail.com>
2023-09-07 06:27:16 +0000

    Allow 256 characters in formation names

--- resources/views/components/formation-name.blade.php
+++ resources/views/components/formation-name.blade.php
@@ -1,11 +1,11 @@
 <div class="form-group">
    <label for="formation_name_1">Formation name:</label>
    @if ($mode=='edit')
-   <input type="text" maxlength=120 size=40 name="formation_name" id="formation_name" required value="{{$formationName}}">
+   <input type="text" maxlength=256 size=45 name="formation_name" id="formation_name" required value="{{$formationName}}">
    <input type="hidden"  name="formation_id" id="formation_id" required value="{{$formationId}}">
    @else
    <input type="text"  name="formation_id" id="formation_id" required value="0">
-   <input type="text" list="formations" maxlength=120 size=45  name="formation_name" id="formation_name">
+   <input type="text" list="formations" maxlength=256 size=45  name="formation_name" id="formation_name">
    <datalist id="formations">
         @foreach($formationList as $key => $formationName)
        <option value="{{ $formationName }}">
